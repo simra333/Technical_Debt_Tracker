c840db0ccaf55c17e533a6f4c2fbe10b43375882
Simra Abid <simra_2000@hotmail.co.uk>
2026-02-23 10:29:51 +0000

    Added acr.tf file

---
+++ terraform/acr.tf
@@ -0,0 +1,14 @@
+resource "azurerm_container_registry" "acr" {
+    name               = "tdtacr"
+    resource_group_name = azurerm_resource_group.rg.name
+    location           = azurerm_resource_group.rg.location
+    sku                = "Basic"
+}
+
+# Attach ACR to AKS
+resource "azurerm_role_assignment" "aks_acr_pull" {
+    scope                = azurerm_container_registry.acr.id
+    role_definition_name = "AcrPull"
+    principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+    skip_service_principal_aad_check = true
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,18 @@ output "aks_cluster_name" {
 output "aks_resource_group" {
   value = azurerm_kubernetes_cluster.aks.resource_group_name
 }
+
+# Output ACR details
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
