6265701bde26c2c4c4a68e001d4f0b2ec375b745
Simra Abid <simra_2000@hotmail.co.uk>
2026-03-10 15:57:48 +0000

    added build and push to ACR stage

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -3,6 +3,7 @@ resource "azurerm_container_registry" "acr" {
     resource_group_name = azurerm_resource_group.rg.name
     location           = azurerm_resource_group.rg.location
     sku                = "Basic"
+    admin_enabled = true
 }
 
 # Attach ACR to AKS
@@ -12,3 +13,4 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
     principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
     skip_service_principal_aad_check = true
 }
+    
