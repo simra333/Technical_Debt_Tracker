2255de3ac3db6b80322bcd04e9f89c4d3f542aa2
Simra Abid <simra_2000@hotmail.co.uk>
2026-03-11 10:40:43 +0000

    modified acr configuration

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -3,15 +3,13 @@ resource "azurerm_container_registry" "acr" {
     resource_group_name = azurerm_resource_group.rg.name
     location           = azurerm_resource_group.rg.location
     sku                = "Basic"
-    admin_enabled = true
 }
 
 # Attach ACR to AKS
 resource "azurerm_role_assignment" "aks_acr_pull" {
     scope                = azurerm_container_registry.acr.id
     role_definition_name = "AcrPull"
-    principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
-    skip_service_principal_aad_check = true
+    principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
 
 # Push permission for CI/CD (Jenkins)
