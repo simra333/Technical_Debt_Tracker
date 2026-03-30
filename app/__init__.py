9b1f45eca8b0bb96fe2bfcf124dab92f8204d151
Simra Abid <simra_2000@hotmail.co.uk>
2026-03-30 11:06:38 +0100

    modified application Insights setup

--- app/__init__.py
+++ app/__init__.py
@@ -10,30 +10,30 @@ import os
 
 db = SQLAlchemy()                           # Create the database object
 
-# Logging setup
-logger = logging.getLogger(__name__)
-logger.addHandler(
-    AzureLogHandler(
-        connection_string=os.environ['APPLICATIONINSIGHTS_CONNECTION_STRING']
-    )
-)
-
-logger.warning("Application Insights is working!")
-
 def create_app():
     app=Flask(__name__)
     app.config.from_object(Config)          # Load configuration
 
     db.init_app(app)                        # Initialise the database with this app
 
-    # Request Tracking setup
-    FlaskMiddleware(
-        app,
-        exporter=AzureExporter(
-            connection_string=os.environ['APPLICATIONINSIGHTS_CONNECTION_STRING']
-        ),
-        sampler=ProbabilitySampler(rate=1.0)
-    )
+    # Logging setup
+    connection_string = os.environ.get('APPLICATIONINSIGHTS_CONNECTION_STRING')
+    if connection_string:
+        logger = logging.getLogger('app')
+
+        if not logger.hasHandlers():  # Avoid adding multiple handlers in development
+            logger.addHandler(
+                AzureLogHandler(connection_string=connection_string)
+            )
+        # Request Tracking setup
+        FlaskMiddleware(
+            app,
+            exporter=AzureExporter(
+                connection_string=connection_string
+            ),
+            sampler=ProbabilitySampler(rate=1.0)
+        )
+        logger.warning("Application Insights is working!")
 
     from app.routes import api              
     app.register_blueprint(api)
