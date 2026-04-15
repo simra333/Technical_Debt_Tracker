b874ae86cb3c7e5787f0620bbcd6c8ef894b92bd
Simra Abid <simra_2000@hotmail.co.uk>
2026-04-15 10:54:45 +0100

    fixed config.py file

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ class Config:
     # Database configuration
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL',
-        'sqlite:///technical_debt.db')
+        'sqlite:////app/instance/technical_debt.db')
 
     # Other configurations settings
-    DEBBUG = True
+    DEBUG = True
 
     FEATURE_FLAGS = {
         "CATEGORY_DROPDOWN": os.getenv("FF_CATEGORY_DROPDOWN", "false").lower() == "true"
